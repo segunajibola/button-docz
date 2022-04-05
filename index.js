b7aef062354f35fb208d1e4e6f6c665f87e22ccf
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-05 14:14:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function  Buttons() {
+    return <Button variant="secondary" size="lg">This is a Bootstrap button</Button>
+}
+
+export {Buttons}
